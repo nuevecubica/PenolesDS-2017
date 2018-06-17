6c846fc1aa91ba90b6c612fcac05666ce469a5e7
alex <alex@nuevecubica.net>
2018-06-17 17:25:23 -0500

    added new page with popup. and popup images

---
+++ Penoles DS-2017/New Group/DerramaEconomica.swift
@@ -0,0 +1,116 @@
+//
+//  DerrameEconomico.swift
+//  Penoles DS-2017
+//
+//  Created by Pablo Gomez Basanta on 6/13/18.
+//  Copyright © 2018 nuevecubica. All rights reserved.
+//
+
+import UIKit
+
+class DerramaEconomicaPage: ImagePage {
+    override var viewController: UIViewController {
+        return DerramaEconomicaViewController(image: UIImage(named: imageName)!)
+    }
+}
+
+class DerramaEconomicaViewController: PageWithButtonsViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let imageScale = view.bounds.width / imageView.image!.size.width
+ 
+        let icon = "plus"
+        
+        //**************************************
+        // Button VALOR GENERADO
+        //**************************************
+        let button_vg = UIButton(frame: CGRect())
+        //button.setTitle("Blah", for: .normal)
+        //button.setTitleColor(.orange, for: .normal)
+        button_vg.addTarget(self, action: #selector(btn_vg_Pressed(_:)), for: .touchUpInside)
+        button_vg.setImage(UIImage(named : icon), for: UIControlState.normal)
+        imageView.addSubview(button_vg)
+        button_vg.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Image coords divided by 3
+        NSLayoutConstraint.activate([
+            button_vg.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 428.5 * imageScale),
+            button_vg.topAnchor.constraint(equalTo: imageView.topAnchor, constant:  314.0 * imageScale)
+            ])
+        //**************************************
+        //**************************************
+        // Button VALOR DISTRIBUIDO
+        //**************************************
+        let button_vd = UIButton(frame: CGRect())
+        //button.setTitle("Blah", for: .normal)
+        //button.setTitleColor(.orange, for: .normal)
+        button_vd.addTarget(self, action: #selector(btn_vd_Pressed(_:)), for: .touchUpInside)
+        button_vd.setImage(UIImage(named : icon), for: UIControlState.normal)
+        imageView.addSubview(button_vd)
+        button_vd.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Image coords divided by 3
+        NSLayoutConstraint.activate([
+            button_vd.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 428.5 * imageScale),
+            button_vd.topAnchor.constraint(equalTo: imageView.topAnchor, constant:  555.0 * imageScale)
+            ])
+        //**************************************
+        //**************************************
+        // Button APORTACIONES A LA COMUNIDAD
+        //**************************************
+        let button_ac = UIButton(frame: CGRect())
+        //button.setTitle("Blah", for: .normal)
+        //button.setTitleColor(.orange, for: .normal)
+        button_ac.addTarget(self, action: #selector(btn_ac_Pressed(_:)), for: .touchUpInside)
+        button_ac.setImage(UIImage(named : icon), for: UIControlState.normal)
+        imageView.addSubview(button_ac)
+        button_ac.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Image coords divided by 3
+        NSLayoutConstraint.activate([
+            button_ac.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 854.9 * imageScale),
+            button_ac.topAnchor.constraint(equalTo: imageView.topAnchor, constant:  284.2 * imageScale)
+            ])
+        //*************************************
+        //**************************************
+        // Button DERRAMA ECONOMICA
+        //**************************************
+        let button_de = UIButton(frame: CGRect())
+        //button.setTitle("Blah", for: .normal)
+        //button.setTitleColor(.orange, for: .normal)
+        button_de.addTarget(self, action: #selector(btn_de_Pressed(_:)), for: .touchUpInside)
+        button_de.setImage(UIImage(named : icon), for: UIControlState.normal)
+        imageView.addSubview(button_de)
+        button_de.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Image coords divided by 3
+        NSLayoutConstraint.activate([
+            button_de.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 854.9 * imageScale),
+            button_de.topAnchor.constraint(equalTo: imageView.topAnchor, constant:  1010.0 * imageScale)
+            ])
+        //*************************************
+    }
+    
+    //*****************************************
+    // PRESS FUNCTIONS
+    //*****************************************
+    @IBAction func btn_vg_Pressed(_ sender: UIButton) {
+        showImagePopup(image: #imageLiteral(resourceName: "valor_genarado") )
+        //showVideoPopup(videoName: "video")
+    }
+    @IBAction func btn_vd_Pressed(_ sender: UIButton) {
+        showImagePopup(image:#imageLiteral(resourceName: "valoe_distributivo"))
+        //showVideoPopup(videoName: "video")
+    }
+    @IBAction func btn_ac_Pressed(_ sender: UIButton) {
+        showImagePopup(image:#imageLiteral(resourceName: "aportaciones"))
+        //showVideoPopup(videoName: "video")
+    }
+    @IBAction func btn_de_Pressed(_ sender: UIButton) {
+        showImagePopup(image:#imageLiteral(resourceName: "derrama_economica"))
+        //showVideoPopup(videoName: "video")
+    }
+}
---
+++ Penoles DS-2017/New Group/manejoDelAgua.swift
@@ -0,0 +1,58 @@
+//
+//  manejoDelAgua.swift
+//  Penoles DS-2017
+//
+//  Created by alex vaught on 6/14/18.
+//  Copyright © 2018 nuevecubica. All rights reserved.
+//
+
+
+import UIKit
+
+class manejodelAguaPage: ImagePage {
+    override var viewController: UIViewController {
+        return manejoDelAguaViewController(image: UIImage(named: imageName)!)
+    }
+}
+
+class manejoDelAguaViewController: PageWithButtonsViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let imageScale = view.bounds.width / imageView.image!.size.width
+        
+        let icon = "plus"
+        
+        //**************************************
+        // Button VALOR GENERADO
+        //**************************************
+        let button_ia = UIButton(frame: CGRect())
+        //button.setTitle("Blah", for: .normal)
+        //button.setTitleColor(.orange, for: .normal)
+        button_ia.addTarget(self, action: #selector(btn_ia_Pressed(_:)), for: .touchUpInside)
+        button_ia.setImage(UIImage(named : icon), for: UIControlState.normal)
+        imageView.addSubview(button_ia)
+        button_ia.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Image coords divided by 3
+        NSLayoutConstraint.activate([
+            button_ia.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 853.0 * imageScale),
+            button_ia.topAnchor.constraint(equalTo: imageView.topAnchor, constant:  400.0 * imageScale)
+            ])
+        //**************************************
+        //**************************************
+       
+    }
+    
+    //*****************************************
+    // PRESS FUNCTIONS
+    //*****************************************
+    @IBAction func btn_ia_Pressed(_ sender: UIButton) {
+        showImagePopup(image: #imageLiteral(resourceName: "indicie_consumo_agua") )
+        //showVideoPopup(videoName: "video")
+    }
+   
+}
+
